5a043cf75f4e77af517c9c003c42bdb9080cf84a
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-01-19 22:40:45 -0800

    Update CUPTI_Conv100.cu

--- examples/CUPTI_Conv100.cu
+++ examples/CUPTI_Conv100.cu
@@ -209,7 +209,7 @@ cuCtxCreate(&context, 0, 0);
 // for(int i = 0; i< 7; i++)
 // {
 //   cudaSetDevice(i);
-  for(int j=0;j<100;j++)
+  for(int j=0;j<10000;j++)
   {
     cupti_profiler::profiler *p= new cupti_profiler::profiler(event_names, metric_names, context);
     struct timeval ts,te;
