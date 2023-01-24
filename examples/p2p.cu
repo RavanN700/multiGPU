be5eeeb0d17a805767e5866edbed1fc563bc7c81
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-01-24 10:14:39 -0800

    Update p2p.cu

--- examples/p2p.cu
+++ examples/p2p.cu
@@ -69,8 +69,10 @@
        exit(EXIT_FAILURE);                                      \
      }                                                          \
    }
+//  __global__ void delay(volatile int *flag,
+//                        unsigned long long timeout_clocks = 10000000) {
  __global__ void delay(volatile int *flag,
-                       unsigned long long timeout_clocks = 10000000) {
+                       unsigned long long timeout_clocks = 1000000000) {                        
    // Wait until the application notifies us that it has completed queuing up the
    // experiment, or timeout and exit, allowing the application to make progress
    long long int start_clock, sample_clock;
