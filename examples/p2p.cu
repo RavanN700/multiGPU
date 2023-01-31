b8921dc816fc99829e77b8ba5103c15bc1d73559
yicheng <yichez16@uci.edu>
2023-01-31 15:03:28 -0800

    update

--- examples/p2p.cu
+++ examples/p2p.cu
@@ -116,12 +116,12 @@ int main(int argc, char **argv) {
 
     printf("Output vector: %d\n", h_C[0]);
     
-    cudaFree(buffers[src]);
-    cudaFree(buffers[det]);
-    cudaFree(buffers[9]);
-    free(h_A);
-    free(h_B);
-    free(h_C);
+    // cudaFree(buffers[src]);
+    // cudaFree(buffers[det]);
+    // cudaFree(buffers[9]);
+    // free(h_A);
+    // free(h_B);
+    // free(h_C);
 
     exit(EXIT_SUCCESS);
  }
