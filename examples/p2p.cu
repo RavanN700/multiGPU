928ebae0efbfaf46d66d74075f7ba2c69e5fb55e
yicheng <yichez16@uci.edu>
2023-01-31 16:21:45 -0800

    update

--- examples/p2p.cu
+++ examples/p2p.cu
@@ -113,9 +113,9 @@ int main(int argc, char **argv) {
 
     double mb = memsize * sizeof(int) / (double)1e6;
     printf("Size of data transfer (MB): %f\n", mb);
-    printf("Output vector V_A[memsize-1] (original value = 1): %d\n", h_A[memsize-1]);
-    printf("Output vector V_B[memsize-1] (original value = 2): %d\n", h_B[memsize-1]);
-    // printf("Output vector V_C[memsize-1] (original value = 3): %d\n", h_C[memsize-1]);
+    printf("Vector V_A[memsize-1] (original value = 1): %d\n", h_A[memsize-1]);
+    printf("Vector V_B[memsize-1] (original value = 2): %d\n", h_B[memsize-1]);
+    // printf("Vector V_C[memsize-1] (original value = 3): %d\n", h_C[memsize-1]);
     
     cudaFree(d_A);
     cudaFree(d_B);
