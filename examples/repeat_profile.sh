ef6f0336de99889d1f09172dd49f4e52528a6e5c
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-02-10 10:58:21 -0800

    Update repeat_profile.sh

--- examples/repeat_profile.sh
+++ examples/repeat_profile.sh
@@ -1,6 +1,7 @@
 for i in $(seq 1 20);
 do
-    nvprof  --profile-from-start off --aggregate-mode off --csv --event-collection-mode continuous -m nvlink_total_data_transmitted,nvlink_total_data_received,nvlink_transmit_throughput,nvlink_receive_throughput ./p2p
+    nvprof  --profile-from-start off --aggregate-mode off --csv --log-file log_$i.csv --event-collection-mode continuous -m nvlink_total_data_transmitted,nvlink_total_data_received,nvlink_transmit_throughput,nvlink_receive_throughput,nvlink_overhead_data_transmitted,nvlink_overhead_data_received ./p2p
+
 done
 
 
