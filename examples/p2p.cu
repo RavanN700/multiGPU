c0d8fd0d1288fcfbd4ad2129fd58dc40fede013a
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-02-09 23:12:17 -0800

    Update p2p.cu

--- examples/p2p.cu
+++ examples/p2p.cu
@@ -96,9 +96,10 @@ int main(int argc, char **argv) {
     cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
 
     // Make src and det device both valid
-    int deviceList[4] = {0,1,2,3};
-    cudaSetValidDevices(deviceList, 2);
+    // int deviceList[4] = {0,1,2,3};
+    // cudaSetValidDevices(deviceList, 2);
     cudaSetDevice(src);
+    cudaDeviceEnablePeerAccess(det);
     int threadsPerBlock = 256;
     int blocksPerGrid = (memsize + threadsPerBlock - 1) / threadsPerBlock;
     
@@ -114,13 +115,11 @@ int main(int argc, char **argv) {
     
 
     // vecadd kernel
-    vecAdd <<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, memsize);
+    // vecAdd <<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, memsize);
 
     // Peer to peer memory copy from device src to device det
-    // cudaSetDevice(src);
-    // cudaMemcpyPeer(d_B, det, d_A, src, size);
-    // cudaSetDevice(det);
-    // cudaMemcpyPeer(d_B, det, d_A, src, size);
+    cudaMemcpyPeer(d_B, det, d_A, src, size);
+
 
     
     // Stop profiler
