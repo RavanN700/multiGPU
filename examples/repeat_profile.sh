6608083458bd22c6a909e3d3112945acaccf59b9
yicheng <yichez16@uci.edu>
2023-02-16 16:50:09 -0800

    update

--- examples/repeat_profile.sh
+++ examples/repeat_profile.sh
@@ -1,4 +1,4 @@
-for i in $(seq 1 5);
+for i in $(seq 1 1);
 do
     nvprof --profile-from-start off --aggregate-mode off --csv --event-collection-mode continuous -m nvlink_total_data_transmitted,nvlink_total_data_received,nvlink_transmit_throughput,nvlink_receive_throughput ./p2p
     nvprof --profile-from-start off --aggregate-mode off --csv --event-collection-mode continuous -m nvlink_overhead_data_transmitted,nvlink_overhead_data_received ./p2p
@@ -11,7 +11,7 @@ do
 done
 
 
-# for i in $(seq 1 10);
+# for i in $(seq 1 1);
 # do
 #     nvprof --profile-from-start off --aggregate-mode off --csv --event-collection-mode kernel -m nvlink_total_data_transmitted,nvlink_total_data_received,nvlink_transmit_throughput,nvlink_receive_throughput,nvlink_overhead_data_transmitted,nvlink_overhead_data_received,nvlink_total_response_data_received,nvlink_user_response_data_received,nvlink_total_write_data_transmitted,nvlink_user_data_transmitted,nvlink_user_data_received,nvlink_user_write_data_transmitted ./p2p
     
