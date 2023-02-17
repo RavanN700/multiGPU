972e468b48e1bc5f295dd3ffb7a68d92f6594b2d
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-02-16 22:49:00 -0800

    Update repeat_profile.sh

--- examples/repeat_profile.sh
+++ examples/repeat_profile.sh
@@ -1,11 +1,11 @@
 for i in $(seq 1 5);
 do
-    nvprof --profile-from-start off --aggregate-mode off --csv --event-collection-mode continuous -m nvlink_total_data_transmitted,nvlink_total_data_received,nvlink_transmit_throughput,nvlink_receive_throughput ./p2p
-    nvprof --profile-from-start off --aggregate-mode off --csv --event-collection-mode continuous -m nvlink_overhead_data_transmitted,nvlink_overhead_data_received ./p2p
-    nvprof --profile-from-start off --aggregate-mode off --csv --event-collection-mode continuous -m nvlink_total_response_data_received,nvlink_user_response_data_received ./p2p
-    nvprof --profile-from-start off --aggregate-mode off --csv --event-collection-mode continuous -m nvlink_user_data_transmitted,nvlink_user_data_received ./p2p
-    nvprof --profile-from-start off --aggregate-mode off --csv --event-collection-mode continuous -m nvlink_total_write_data_transmitted ./p2p
-    nvprof --profile-from-start off --aggregate-mode off --csv --event-collection-mode continuous -m nvlink_user_write_data_transmitted ./p2p
+    nvprof --profile-from-start off --aggregate-mode off --csv --log-file log_continuous_0_$i.csv --event-collection-mode continuous -m nvlink_total_data_transmitted,nvlink_total_data_received,nvlink_transmit_throughput,nvlink_receive_throughput ./p2p
+    nvprof --profile-from-start off --aggregate-mode off --csv --log-file log_continuous_1_$i.csv --event-collection-mode continuous -m nvlink_overhead_data_transmitted,nvlink_overhead_data_received ./p2p
+    nvprof --profile-from-start off --aggregate-mode off --csv --log-file log_continuous_2_$i.csv --event-collection-mode continuous -m nvlink_total_response_data_received,nvlink_user_response_data_received ./p2p
+    nvprof --profile-from-start off --aggregate-mode off --csv --log-file log_continuous_3_$i.csv --event-collection-mode continuous -m nvlink_user_data_transmitted,nvlink_user_data_received ./p2p
+    nvprof --profile-from-start off --aggregate-mode off --csv --log-file log_continuous_4_$i.csv --event-collection-mode continuous -m nvlink_total_write_data_transmitted ./p2p
+    nvprof --profile-from-start off --aggregate-mode off --csv --log-file log_continuous_5_$i.csv --event-collection-mode continuous -m nvlink_user_write_data_transmitted ./p2p
 
 
 done
