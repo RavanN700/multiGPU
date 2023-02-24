b3d9ea1d4d142551d4ee5a411ea9ab9b94abbdb0
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-02-24 11:18:35 -0800

    Update repeat_profile.sh

--- examples/repeat_profile.sh
+++ examples/repeat_profile.sh
@@ -16,7 +16,7 @@ do
     # nvprof --profile-from-start off --aggregate-mode off --csv --log-file ./data/log_kernel_$i.csv --event-collection-mode kernel -m nvlink_total_data_transmitted,nvlink_total_data_received,nvlink_transmit_throughput,nvlink_receive_throughput,nvlink_overhead_data_transmitted,nvlink_overhead_data_received,nvlink_total_response_data_received,nvlink_user_response_data_received,nvlink_total_write_data_transmitted,nvlink_user_data_transmitted,nvlink_user_data_received,nvlink_user_write_data_transmitted ./p2p
     
 
-    nvprof --profile-from-start on --aggregate-mode off --csv --event-collection-mode kernel -m nvlink_total_data_transmitted,nvlink_total_data_received,nvlink_transmit_throughput,nvlink_receive_throughput,nvlink_overhead_data_transmitted,nvlink_overhead_data_received,nvlink_total_response_data_received,nvlink_user_response_data_received,nvlink_total_write_data_transmitted,nvlink_user_data_transmitted,nvlink_user_data_received,nvlink_user_write_data_transmitted ./p2p i
+    nvprof --profile-from-start off --aggregate-mode off --csv --event-collection-mode kernel -m nvlink_total_data_transmitted,nvlink_total_data_received,nvlink_transmit_throughput,nvlink_receive_throughput,nvlink_overhead_data_transmitted,nvlink_overhead_data_received,nvlink_total_response_data_received,nvlink_user_response_data_received,nvlink_total_write_data_transmitted,nvlink_user_data_transmitted,nvlink_user_data_received,nvlink_user_write_data_transmitted ./p2p i
     # nvprof --profile-from-start on --aggregate-mode off --csv --event-collection-mode kernel -m nvlink_total_data_transmitted ./p2p
 
 
